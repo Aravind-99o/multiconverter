0772dfd769a9be39117a2b84441def09c07bd570
Aravind-99o <iaravind.99o@gmail.com>
2024-11-22 16:54:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,11 +16,6 @@ if unit=="Length":
     "Select Conversion",
     ("Select an option", "Length Centimeter to Meter", "Length Meter to Centimeter"),
     )
-
-    if conversion == "Select an option":
-    st.write("Please select a valid option.")
-    else:
-    st.write(f"You selected: {conversion}")
-if convertion=="Length Centimeter to Meter":
-    age = st.slider("Select Length in Centimeter ", 0, 1000, 100)
+    if convertion=="Length Centimeter to Meter":
+        cm = st.slider("Select Length in Centimeter ", 0, 1000, 100)
     
