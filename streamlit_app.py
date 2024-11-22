8baa31492b84cbd86db3f419f81782f0cd20205d
Aravind-99o <iaravind.99o@gmail.com>
2024-11-22 16:57:27 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,6 +15,7 @@ if unit=="Length":
    conversion = st.selectbox(
     "Select Conversion",
     ("Select an option", "Length Centimeter to Meter", "Length Meter to Centimeter"),
+    index=0  # Explicitly set the default index to the first item
     )
     if convertion=="Length Centimeter to Meter":
         cm = st.slider("Select Length in Centimeter ", 0, 1000, 100)
