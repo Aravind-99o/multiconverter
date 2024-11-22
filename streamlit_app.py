1ece6364220de76ad096dba3e0994008fd450538
Aravind-99o <iaravind.99o@gmail.com>
2024-11-22 16:13:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,4 @@ import streamlit as st
 
 st.title('Multi Converter')
 
-st.write('This multi-converter application 
-Allows users to easily convert between different units of length, currency, and weight.
-The modular design makes it easy to extend with additional conversion functionalities as needed.')
+st.write('This multi-converter application allows users to easily convert between different units of length, currency, and weight.The modular design makes it easy to extendwith additional conversion functionalities as needed.')
