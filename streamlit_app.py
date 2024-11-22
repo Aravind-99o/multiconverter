48d08331252703399734c8a82bee087e828da517
Aravind-99o <iaravind.99o@gmail.com>
2024-11-22 16:22:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,9 @@ st.title('Multi Converter')
 
 st.write('This multi-converter application allows users to easily convert between different units of length, currency, and weight.The modular design makes it easy to extendwith additional conversion functionalities as needed.')
 
-st.selectbox(label, options, index=0, format_func=special_internal_function, key=None, help=None, on_change=None, args=None, kwargs=None, *, placeholder="Choose an option", disabled=False, label_visibility="visible")
+unit = st.selectbox(
+    "select the unit to convert",
+    ("Length", "Weight", "Currency"),
+)
+
+st.write("You selected:", unit)
