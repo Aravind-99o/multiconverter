708e250389e8248be832ba5a89d06ed1d7e3272e
Aravind-99o <iaravind.99o@gmail.com>
2024-11-22 17:17:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ else:
         if convertion == "Select an option":
             st.write("Please select a valid convertion.")
         else:
-            st.write(f"You selected: {convertion}")
             if convertion=="Length Centimeter to Meter":
                 cm = st.slider("Select Length in Centimeter ", 0, 1000, 100)
     
+            
